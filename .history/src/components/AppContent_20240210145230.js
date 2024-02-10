bf2b0e1552267b738bf98eddc6156a20580919f9
Lukorito <jonesdeelder@gmail.com>
2024-02-10 15:20:19 +0300

    add FE components

---
+++ .history/src/components/AppContent_20240210145230.js
@@ -0,0 +1,26 @@
+import * as React from 'react'
+
+export default class AppContent extends React.Component {
+    // constructor(props) {
+    //     super(props)
+    //     this.state = {
+    //         token: null,
+    //         username: null
+    //     }
+    // }
+    // componentDidMount() {
+    //     this.setState({
+    //         token: localStorage.getItem('token'),
+    //         username: localStorage.getItem('username')
+    //     })
+    // }
+    render() {
+        return (
+            <div>
+                <WelcomeContent/>
+                {/* <p>Token: {this.state.token}</p>
+                <p>Username: {this.state.username}</p> */}
+            </div>
+        )
+    }
+}
---
+++ .history/src/components/App_20240210150015.js
@@ -0,0 +1,16 @@
+import './App.css'
+import logo from '../logo.svg'
+import Header from './Header'
+import AppContent from './AppContent'
+
+function App() {
+
+    return (
+        <div>
+        <Header pageTitle="Frontend authenticated with JWT" logoSrc={logo} />
+        <AppContent />
+        </div>
+    );
+}
+
+export default App;
